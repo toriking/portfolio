9ba3705f69228db0a1a8c77753256729c44a91ab
toriking <54404273+toriking@users.noreply.github.com>
2020-09-10 21:12:30 +0900

    Add files via upload

--- js/bubble-burst.js
+++ js/bubble-burst.js
@@ -2,6 +2,7 @@ const triggeri = document.getElementsByClassName("bable-item");
 const triggers = Array.from(triggeri);
 triggers.forEach(function(target) {
     target.addEventListener('click', () => {
+        
         target.style.display = 'none';
         audio = new Audio;
         audio.src = "static/images/bubble-burst.mp3";
@@ -10,4 +11,13 @@ triggers.forEach(function(target) {
             target.style.display = 'block';
             },5000);
             },false);
-            });
+        target.addEventListener('touchstart', ()=>{
+            target.style.display = 'none';
+            audio = new Audio;
+            audio.src = "static/images/bubble-burst.mp3";
+            audio.play();
+            setTimeout(()=>{
+                target.style.display = 'block';
+                },5000);
+        },false)    
+        });
