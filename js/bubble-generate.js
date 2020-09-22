f6c168edc219172a3b5d6b2914d7e8a42187f1bb
toriking <54404273+toriking@users.noreply.github.com>
2020-09-22 19:13:19 +0900

    Delete bubble-generate.js

--- js/bubble-generate.js
+++
@@ -1,9 +0,0 @@
-// var items =50;
-// for (var i=0; i<=items; i++){
-//     var moveVal = Math.ceil(Math.random()*50);
-//     var posVal = Math.ceil(Math.random()*50);
-//     var scaleVal = Math.ceil(Math.random()*10);
-//     var shakeVal = Math.ceil(Math.random()*5);
-//     $(".bable-frame").append('<div class="move'+moveVal+' pos'+posVal+'"><div class="scale'+scaleVal+'"><div class="bable-item shake'+shakeVal+'"></div></div>');
-// }
-
