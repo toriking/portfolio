25fa7153f5516f82b84e55ac46cbe5b5cc039a8b
toriking <54404273+toriking@users.noreply.github.com>
2020-09-22 19:10:02 +0900

    Add files via upload

--- js/fadein.js
+++ js/fadein.js
@@ -1,18 +1,23 @@
 const startTime = performance.now();
 const fades = document.querySelectorAll('.circle-content,.fade')
+const topScroll = document.querySelector('.top-scroll');
 const options = {
     root: null,
     rootMargin: '0px',
-    threshold: 0.20
+    threshold: 0
   }
 const observer = new IntersectionObserver((changes)=>{
   changes.forEach(change => {
   if(change.isIntersecting){
     change.target.classList.add('show');
-  } else {
-    change.target.classList.remove('show');
   }
 });
+const topB = window.pageYOffset;
+if (topB > 600) {
+    topScroll.classList.add('show');
+} else {
+ topScroll.classList.remove('show');
+}
 }, options);
 for(let i =0 ; i < fades.length; i++){
   observer.observe(fades[i]);
